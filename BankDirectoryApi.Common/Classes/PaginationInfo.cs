1c2a71817203fcf9a9adb41678bdeab6bc8bdf10
KararMohammed2009 <karar.m2009@gmail.com>
2025-03-27 00:31:26 +0300

    update pagination

---
+++ BankDirectoryApi.Common/Classes/PaginationInfo.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDirectoryApi.Common.Classes
+{
+    public class PaginationInfo
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string SortColumn { get; set; } = "Id"; 
+        public string SortDirection { get; set; } = "asc"; 
+    }
+}
---
+++ BankDirectoryApi.Common/Classes/PaginationResult.cs
@@ -0,0 +1,20 @@
+﻿using BankDirectoryApi.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDirectoryApi.Common.Classes
+{
+    public class PaginationResult<T> 
+    {
+        public List<T>? Items { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+}
